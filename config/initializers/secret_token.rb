0d92efe0e4004a1123644afbf6119100c437fca9
Sergei O. Udalov <sergei.udalov@gmail.com>
2012-03-21 07:39:37 +0400

    rails init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BibleComments::Application.config.secret_token = '43793c73a00cfa6cba48bc6011625f24c6e449518291bca32600dbebb6eeaac375dda386edb5fca7c9320c60fae353307a6233ef4b3ff4d198e7727624d1880c'
